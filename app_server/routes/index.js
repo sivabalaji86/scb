ec0b34a46589d5b217dbc878e70361257d220456
Balaji Sivaprakasam <balaji.sivaprakasam@sc.com>
2016-11-23 12:00:15 +0800

    V2 + Abi :-)

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/abi', ctrlLocations.abiMsg);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;
